a79a8d1f22696e62151c88539d17691ce6d0d80c
Evan Schmidt <eszqy@umsystem.edu>
2025-09-08 17:53:16 -0500

    Basic functionality of opening dino window and jumping a few times

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-#pleaseeeeeeeee work
-#Nuh uh
+import pyautogui
+import webbrowser
+import time
+
+
+
+browser = webbrowser.get()
+browser.open('https://chromedino.com/')
+
+time.sleep(3) 
+
+
+for _ in range(1000):  
+    pyautogui.press('up')
+    time.sleep(0.1)  
