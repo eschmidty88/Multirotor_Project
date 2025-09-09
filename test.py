247400993bba17e0ba86154bd4db8e7441293208
Evan Schmidt <eszqy@umsystem.edu>
2025-09-08 19:46:15 -0500

    Added restart.png and made it better

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import numpy as np
 browser = webbrowser.get()
 browser.open('https://chromedino.com/')
 
-time.sleep(3) 
+time.sleep(1) 
 
 # Locate the dinosaur on the screen
 dino_location = pyautogui.locateOnScreen('dino.png') 
@@ -20,15 +20,20 @@ pyautogui.center(dino_location)
 reference_color = getpixelcolor.average(770, 420, 10, 10)
 
 # Mpve the mouse to center of scanning region
-pyautogui.moveTo(770, 400)
+pyautogui.moveTo(790, 400)
 
 # Start game
 pyautogui.press('up')
 
 
-# Loop
+
+# Loop 948, 394
 while True:
-    current_color = getpixelcolor.average(770, 420, 10, 10)
+    current_color = getpixelcolor.average(790, 420, 10, 10)
     if not np.array_equal(current_color, reference_color):
         pyautogui.press('up')
         time.sleep(0.05)
+    
+    restart_color = getpixelcolor.average(948, 384, 10, 10)
+    if not np.array_equal(restart_color, reference_color):
+        pyautogui.press('up')
