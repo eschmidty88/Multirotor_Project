44322e07f70fc48918a769774ec534e0c9ff3aa1
Evan Schmidt <eszqy@umsystem.edu>
2025-09-08 19:18:34 -0500

    We have a semi-viable program lmao

--- test.py
+++ test.py
@@ -1,15 +1,34 @@
 import pyautogui
 import webbrowser
 import time
+import getpixelcolor
+import numpy as np
 
 
-
+# Open the Chrome Dino game in the web browser
 browser = webbrowser.get()
 browser.open('https://chromedino.com/')
 
 time.sleep(3) 
 
+# Locate the dinosaur on the screen
+dino_location = pyautogui.locateOnScreen('dino.png') 
+print(dino_location)
 
-for _ in range(1000):  
-    pyautogui.press('up')
-    time.sleep(0.1)  
+# Move the mouse to the center of the dinosaur
+pyautogui.center(dino_location)
+reference_color = getpixelcolor.average(770, 420, 10, 10)
+
+# Mpve the mouse to center of scanning region
+pyautogui.moveTo(770, 400)
+
+# Start game
+pyautogui.press('up')
+
+
+# Loop
+while True:
+    current_color = getpixelcolor.average(770, 420, 10, 10)
+    if not np.array_equal(current_color, reference_color):
+        pyautogui.press('up')
+        time.sleep(0.05)
