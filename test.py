09ac586cf4da3b902b8a096373692b5b06008372
Evan Schmidt <eszqy@umsystem.edu>
2025-09-15 18:10:05 -0500

    I have no idea anymore bro

--- test.py
+++ test.py
@@ -12,35 +12,50 @@ browser.open('https://chromedino.com/')
 time.sleep(3) 
 
 # Locate the dinosaur on the screen
-dino_location = pyautogui.locateOnScreen('dino.png') 
-print(dino_location)
+#dino_location = pyautogui.locateOnScreen('dino.png') 
+#print(dino_location)
 
 # Move the mouse to the center of the dinosaur
-pyautogui.center(dino_location)
-reference_color = getpixelcolor.average(760, 420, 10, 10)
+#pyautogui.center(dino_location)
+reference_color = getpixelcolor.pixel(185, 347)
+# Move the mouse to center of scanning region
+x = 730
+#770
 
-# Mpve the mouse to center of scanning region
-pyautogui.moveTo(745, 431)
+y = 430
+width = 30 
+height = 10
+#0.017
+jump_length = 0.016
+downtime = 0.06
+
+pyautogui.moveTo(x, y)
+
 
 # Start game
 pyautogui.press('up')
-# pyautogui.mouseInfo()
 
 
 # Loop 948, 394
+
 while True:
-    current_color = getpixelcolor.average(745, 424, 10, 10)
+    current_color = getpixelcolor.average(x, y, width , height)
     if not np.array_equal(current_color, reference_color):
+    
+
         # pyautogui.press('up')
         pyautogui.keyDown('up')
-        time.sleep(0.04)
+        time.sleep(jump_length)
         pyautogui.keyUp('up')
-        time.sleep(0.002)
+        #time.sleep(downtime)
         pyautogui.keyDown('down')
-        time.sleep(0.08)
+        time.sleep(downtime)
         pyautogui.keyUp('down')
-        #time.sleep(0.02)
-    
-    restart_color = getpixelcolor.average(948, 384, 10, 10)
+
+
+    restart_color = getpixelcolor.average(948, 384, 10, 15)
     if not np.array_equal(restart_color, reference_color):
         pyautogui.press('up')
+        time.sleep(0.5)
+
+
